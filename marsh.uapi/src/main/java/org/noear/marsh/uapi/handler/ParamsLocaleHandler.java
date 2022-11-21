19b458a1d1170b65bad6e74b1fea8f8710c97f9c
noear <noear@live.cn>
2022-11-21 22:16:25 +0800

    1.2.4

--- marsh.uapi/src/main/java/org/noear/marsh/uapi/handler/ParamsLocaleHandler.java
+++ marsh.uapi/src/main/java/org/noear/marsh/uapi/handler/ParamsLocaleHandler.java
@@ -6,16 +6,28 @@ import org.noear.solon.core.handle.Handler;
 import org.noear.solon.i18n.LocaleUtil;
 import org.noear.marsh.uapi.common.Attrs;
 
+import java.util.Locale;
+
 /**
  * 国际化地区设定拦截器
  */
 public class ParamsLocaleHandler implements Handler {
+    Locale locale;
+
+    public ParamsLocaleHandler() {
+    }
+
+    public ParamsLocaleHandler(Locale locale) {
+        this.locale = locale;
+    }
 
     @Override
     public void handle(Context ctx) throws Throwable {
         String lang = ctx.param(Attrs.g_lang);
         if (Utils.isNotEmpty(lang)) {
             ctx.setLocale(LocaleUtil.toLocale(lang));
+        } else if (locale != null) {
+            ctx.setLocale(locale);
         }
     }
 }
