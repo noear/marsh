d29ab583f6eb8343593df6a185bcf184df4c1da0
noear <noear@live.cn>
2023-04-22 23:27:59 +0800

    1.3.13

--- marsh.uapi/src/main/java/org/noear/marsh/uapi/handler/StartAfterLogHandler.java
+++ marsh.uapi/src/main/java/org/noear/marsh/uapi/handler/StartAfterLogHandler.java
@@ -1,6 +1,9 @@
 package org.noear.marsh.uapi.handler;
 
+import org.noear.marsh.base.GlobalConfig;
 import org.noear.snack.ONode;
+import org.noear.solon.Solon;
+import org.noear.solon.Utils;
 import org.noear.solon.core.handle.Context;
 import org.noear.solon.core.handle.Handler;
 import org.slf4j.Logger;
@@ -11,18 +14,46 @@ import org.slf4j.LoggerFactory;
  */
 public class StartAfterLogHandler implements Handler {
     Logger logger;
+    int inputLimitSize;
 
     public StartAfterLogHandler() {
-        logger = LoggerFactory.getLogger(EndBeforeLogHandler.class);
+        this(null);
     }
 
     public StartAfterLogHandler(String loggerName) {
-        logger = LoggerFactory.getLogger(loggerName);
+        if (Utils.isNotEmpty(loggerName)) {
+            logger = LoggerFactory.getLogger(loggerName);
+        } else {
+            logger = LoggerFactory.getLogger(EndBeforeLogHandler.class);
+        }
+
+        inputLimitSize = GlobalConfig.logInputLimitSize();
+        Solon.cfg().onChange((k, v) -> {
+            //自动更新
+            if (GlobalConfig.KEY_inputLimitSize.equals(k)) {
+                inputLimitSize = Integer.parseInt(v);
+            }
+        });
     }
 
 
     @Override
     public void handle(Context ctx) throws Throwable {
-        logger.info("> Header: {}\n> Param: {}\n> Body: {}", ONode.stringify(ctx.headerMap()), ONode.stringify(ctx.paramMap()), ctx.body());
+        logger.info("> Header: {}\n> Param: {}\n> Body: {}",
+                limit(ONode.stringify(ctx.headerMap())),
+                limit(ONode.stringify(ctx.paramMap())),
+                limit(ctx.body()));
+    }
+
+    private String limit(String str) {
+        if (str == null) {
+            return "";
+        }
+
+        if (str.length() > inputLimitSize) {
+            return str.substring(0, inputLimitSize);
+        } else {
+            return str;
+        }
     }
 }
